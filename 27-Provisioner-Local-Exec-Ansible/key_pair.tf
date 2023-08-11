82c3464c8a4cf774a81aa1440104c767ac824319
Amit Vashist <amitvashist7@outlook.com>
2023-08-11 10:28:00 +0000

    Key Permission

--- 27-Provisioner-Local-Exec-Ansible/key_pair.tf
+++ 27-Provisioner-Local-Exec-Ansible/key_pair.tf
@@ -7,13 +7,13 @@ resource "tls_private_key" "example_ssh" {
 resource "local_file" "write_private_key" {
   content         = tls_private_key.example_ssh.private_key_pem
   filename        = "${path.root}/.terrform/.ssh/admin_rsa"
-  file_permission = 0600
+  file_permission = 0400
 }
 
 resource "local_file" "write_public_key" {
   content         = tls_private_key.example_ssh.public_key_openssh
   filename        = "${path.root}/.terrform/.ssh/admin_rsa.pub"
-  file_permission = 0600
+  file_permission = 0400
 }
 
 resource "aws_key_pair" "example_ssh_keypair" {
